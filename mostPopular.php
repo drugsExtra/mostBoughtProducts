6f8e8ffc02cba67462063faacb5d885a93658150
alex-melnik <rabota.melnik@gmail.com>
2019-01-27 19:32:18 +0300

    Create mostPopular.php

---
+++ mostPopular.php
@@ -0,0 +1,88 @@
+<?php
+/**
+* MODX
+* 
+*/
+ $scriptProperties=[];
+// получим массив объектов заказов
+$orders = $modx->getCollection('msOrder');
+$list=[];
+// запишем в массив list идентификаторы всех заказов
+foreach ($orders as $order){
+    $list[]=$order->get('id');
+}
+
+$products=[];
+$miniShop2 = $modx->getService('miniShop2');
+$miniShop2->initialize($modx->context->key);
+/** @var pdoFetch $pdoFetch */
+if (!$modx->loadClass('pdofetch', MODX_CORE_PATH . 'components/pdotools/model/pdotools/', false, true)) {
+    return false;
+}
+$pdoFetch = new pdoFetch($modx, $scriptProperties);
+//для кадлого заказа получаем товары этого заказа и заносим в массив products (ключем является идентификатор товара, а значением будет количество раз когда его купили)
+foreach ($list as $id) {
+
+if (!$order = $modx->getObject('msOrder', $id)) {
+    //return $modx->lexicon('ms2_err_order_nf');
+}
+$where = array(
+    'msOrderProduct.order_id' => $id,
+);
+
+// Include products properties
+$leftJoin = array(
+    'msProduct' => array(
+        'class' => 'msProduct',
+        'on' => 'msProduct.id = msOrderProduct.product_id',
+    )
+);
+
+// Select columns
+$select = array(
+    'msProduct' => !empty($includeContent)
+        ? $modx->getSelectColumns('msProduct', 'msProduct')
+        : $modx->getSelectColumns('msProduct', 'msProduct', '', array('content'), true),
+   
+);
+
+// Tables for joining
+$default = array(
+    'class' => 'msOrderProduct',
+    'where' => $where,
+    'leftJoin' => $leftJoin,
+    'select' => $select,
+    'joinTVsTo' => 'msProduct',
+    'sortby' => 'msOrderProduct.id',
+    'sortdir' => 'asc',
+    'groupby' => 'msOrderProduct.id',
+    'fastMode' => false,
+    'limit' => 0,
+    'return' => 'data',
+    'decodeJSON' => true,
+    'nestedChunkPrefix' => 'minishop2_',
+);
+// Merge all properties and run!
+$pdoFetch->setConfig(array_merge($default, $scriptProperties), true);
+$rows = $pdoFetch->run();
+
+
+
+foreach ($rows as $product) {
+    if ($product['id']>0){
+   $products[(int)$product['id']]++;
+    }
+}
+
+} //end foreach
+// сортируем массив по значению, чтобы первыми были те записи у кого значение больше
+arsort($products);
+// формируем ссылку на товар, находим его имя и выводим на экран
+foreach ($products as $id=>$oneProduct){
+    $url = $modx->makeUrl($id);
+    $test=$modx->getObject('modResource',$id);
+    $name = $test->get('pagetitle');
+    echo "<div><a href='".$url."'>".$name."</a> Купили <strong>".$oneProduct."</strong> раз(а)</div>";
+    //echo $name . ' / ' . $url . '</br>';
+}
+//return var_dump($prod);
